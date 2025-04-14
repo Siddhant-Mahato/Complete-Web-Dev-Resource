53e00b1d5ddde67402c0eee7963b81cb6381435e
Siddhant Mahato <127202564+Siddhant-Mahato@users.noreply.github.com>
2025-04-14 11:55:23 +0530

    ABOUT JS

--- 1.4 - JS/2.5 - Js-DOM1/14-appendChild.js
+++ 1.4 - JS/2.5 - Js-DOM1/14-appendChild.js
@@ -5,7 +5,9 @@ let newElement = document.createElement('h1') //create h1
 
 // add content in h1 
 let content = document.createTextNode("This is Heading");
+
 newElement.appendChild(content);
+
 
 
 parent.appendChild(newElement);

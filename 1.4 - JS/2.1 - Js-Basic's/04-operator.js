3768c9266e82cbc4466369f0790756dd699fb9dc
Siddhant Mahato <127202564+Siddhant-Mahato@users.noreply.github.com>
2025-06-23 13:10:19 +0530

    ABOUT JS

--- 1.4 - JS/2.1 - Js-Basic's/04-operator.js
+++ 1.4 - JS/2.1 - Js-Basic's/04-operator.js
@@ -21,6 +21,7 @@ console.log(num1 % num2);
 console.log("Post and Pre Increment and Decrement");
 
 console.log(num1++ + ++num2); // 11 21 31
+
 console.log(++num1 + num2--); // 12 20 33 
 console.log(num1-- - num2--); // 11 19 -8
 console.log(num1-- - --num2); // 10 18 -7

4bcf9a521177fffd734fe694a8bee5ba4f88549e
Siddhant Mahato <127202564+Siddhant-Mahato@users.noreply.github.com>
2025-11-20 14:02:26 +0530

    ABOUT JS

--- 1.4 - JS/2.1 - Js-Basic's/star.js
+++ 1.4 - JS/2.1 - Js-Basic's/star.js
@@ -12,7 +12,7 @@ console.log(a);
 
 
 # let a = 90;
-# console.log(a);  // This can't be done in any programming language i.r redeclaration not allowed
+# // console.log(a);  // This can't be done in any programming language i.r redeclaration not allowed
 
 
 var b = 90;
@@ -30,6 +30,7 @@ const c = 90;
 console.log(c);
 
 # // c = 900;
+
 // console.log(c);  NOT POSSIBLE
 
 
@@ -39,6 +40,7 @@ console.log(d);
 
 d = "SIDDHANT";
 console.log(d);
+
 
 
 

6e8acc322f6fdcfbd0eb36ca0df04fde0dd0c798
Siddhant Mahato <127202564+Siddhant-Mahato@users.noreply.github.com>
2026-02-13 20:48:39 +0530

    ABOUT JS

--- 1.4 - JS/2.2 - Js-Object's/11-objectCloningBy-Assign.js
+++ 1.4 - JS/2.2 - Js-Object's/11-objectCloningBy-Assign.js
@@ -12,7 +12,8 @@ const rectangle =
     console.log("Drawing rectangle");
   },
 
-  area: function () {
+  area: function () 
+  {
     console.log(`Area of Rectangle: ${rectangle.length * rectangle.breadth}`);
   },
 };

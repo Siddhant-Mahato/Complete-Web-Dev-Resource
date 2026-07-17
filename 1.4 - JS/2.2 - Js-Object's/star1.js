1a0b033d3f75ea0282d59d5d7dbb67a5b5dcacd2
Siddhant Mahato <127202564+Siddhant-Mahato@users.noreply.github.com>
2026-07-17 21:28:53 +0530

    ABOUT JS

--- 1.4 - JS/2.2 - Js-Object's/star1.js
+++ 1.4 - JS/2.2 - Js-Object's/star1.js
@@ -5,7 +5,8 @@ function createRec()
         {
                 length: 2,
                 breadth: 4,
-                draw: function () {
+                draw: function ()
+                {
                         console.log("Drawing rectangle");
         // }
 //     };

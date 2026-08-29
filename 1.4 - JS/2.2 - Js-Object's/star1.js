66820f991c9a18f86901432400837496d909bcce
Siddhant Mahato <127202564+Siddhant-Mahato@users.noreply.github.com>
2026-08-29 16:36:42 +0530

    ABOUT JS

--- 1.4 - JS/2.2 - Js-Object's/star1.js
+++ 1.4 - JS/2.2 - Js-Object's/star1.js
@@ -28,7 +28,7 @@ function Rec(len,bre)
 e
     this.draw = function()
     {
-        console.log("Star Siddhant Is The Great Pe");
+        console.log("Star Siddhant Is The Great Per");
     }
 }
 

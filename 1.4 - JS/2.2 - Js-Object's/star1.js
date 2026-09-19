a9dad119bc5e74d07b77fd9ad24c7d35ab817616
Siddhant Mahato <127202564+Siddhant-Mahato@users.noreply.github.com>
2026-09-20 00:51:41 +0530

    ABOUT JS

--- 1.4 - JS/2.2 - Js-Object's/star1.js
+++ 1.4 - JS/2.2 - Js-Object's/star1.js
@@ -28,7 +28,7 @@ function Rec(len,bre)
 e
     this.draw = function()
     {
-        console.log("Star Siddhant Is The Great Person Ever Been Met In The En");
+        console.log("Star Siddhant Is The Great Person Ever Been Met In The Ent");
     }
 }
 
